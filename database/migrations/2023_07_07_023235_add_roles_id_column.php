70948c44247fe2c9342f1891e73e70f6b42125bc
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2023-07-08 15:02:14 +0700

    ganti migration

--- database/migrations/2023_07_07_023235_add_roles_id_column.php
+++ database/migrations/2023_07_07_023235_add_roles_id_column.php
@@ -14,9 +14,7 @@ class AddRolesIdColumn extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('roles_id');
-
-            $table->foreign('roles_id')->references('id')->on('roles');
+            $table->string('roles');
         });
     }
 
@@ -28,8 +26,7 @@ class AddRolesIdColumn extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropForeign(['roles_id']);
-            $table->dropColumn('roles_id');
+            $table->dropColumn('roles');
         });
     }
 }
